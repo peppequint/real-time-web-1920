11d3cd701285c933c836627a58eb57e86ea6b499
Peppe-Quint <peppequintb@gmail.com>
2020-04-07 11:51:22 +0200

    init server

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const path = require('path');
+
+const routes = require('./routes/routes');
+
+// const sockets = require('./controllers/socket');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.use(express.static(path.join(__dirname, '/static')));
+app.use(routes);
+
+io.on('connection', (socket) => {
+  console.log('An user is connected!');
+});
+
+http.listen(port, () => {
+  console.log(`Real time application running on port ${port}`);
+});
