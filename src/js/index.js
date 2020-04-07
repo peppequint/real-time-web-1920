4db5a068f2f9a525d3a41a2c71548596da12832c
Peppe-Quint <peppequintb@gmail.com>
2020-04-07 12:05:14 +0200

    client side socket

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,20 @@ console.log('index.js');
 
 const socket = io();
 
-// (function() {
+const form = document.querySelector('form');
+const message = document.querySelector('#message');
+const messages = document.querySelector('.message-list');
 
-// }));
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  socket.emit('chat', message.value);
+  message.value = '';
+  return false;
+});
+
+socket.on('chat', (message) => {
+  const li = document.createElement('li');
+  li.append(message);
+  messages.append(li);
+});
