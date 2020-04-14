fb72e434eb19067d9c6afde14c316c6ab61a594b
Peppe-Quint <peppequintb@gmail.com>
2020-04-14 11:33:06 +0200

    deleted chat example

--- app.js
+++ app.js
@@ -17,55 +17,6 @@ app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, '/static')));
 app.use(routes);
 
-io.on('connection', (socket) => {
-  let username = 'test-user';
-  let encrypted = false;
-
-  console.log(`${username} is connected!`);
-  socket.emit('server message', `${username}, you are connected.`);
-  socket.broadcast.emit('server message', `${username} is in the room!`);
-
-  socket.on('username', (name) => {
-    username = name;
-
-    console.log(username);
-    io.emit('server message', `${username} is in the room!`);
-  });
-
-  socket.on('encrypted', (username) => {
-    console.log(`${username} encrypted the chat!`);
-    encrypted = true;
-    console.log(encrypted);
-    socket.emit('server message', `You have encrypted the messages!`);
-    socket.broadcast.emit('server message', `${username} has encrypted the messages!`);
-    return encrypted;
-  });
-
-  socket.on('chat', (message) => {
-    if (encrypted === true) {
-      encryptedMessage(message);
-      console.log('It is encrypted!');
-      io.emit('server message', `You have encrypted the messages!`);
-      socket.broadcast.emit('server message', `You can't see the messages!`);
-    } else if (encrypted === false) {
-      console.log(message);
-      io.emit('chat', `${username}: ${message}`);
-    }
-  });
-
-  socket.on('disconnect', () => {
-    console.log(`${username} is disconnected!`);
-    io.emit('server message', `${username} left the room!`);
-  });
-});
-
-function encryptedMessage(message) {
-  // iets doen met de berichten, api?
-  console.log('Dit is de encrypted: ', message);
-
-  return message;
-}
-
 http.listen(port, () => {
   console.log(`Real time application running on port ${port}`);
 });
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,4 @@
-// const data = require('./../data/data.json');
-
 async function index(req, res) {
-  // for (const players in data) {
-  //   return players;
-  // }
   return res.render('pages/index');
 }
 
