38625fb8052567b59f505a32685784c7755382f0
Peppe-Quint <peppequintb@gmail.com>
2020-04-19 17:06:19 +0200

    auth spotify

---
+++ middleware/callback.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+const fetch = require('node-fetch');
+const queryString = require('query-string');
+
+async function callback(req, res) {
+  const code = req.query.code;
+  const queryObject = {
+    grant_type: 'authorization_code',
+    code: code,
+    redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+  };
+
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${encode(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_SECRET_ID}`)}`,
+    },
+  };
+
+  const query = queryString.stringify(queryObject);
+  const url = `https://accounts.spotify.com/api/token?${query}`;
+
+  try {
+    const response = await fetch(url, fetchOptions);
+    const data = await response.json();
+
+    res.cookie('access_token', data.access_token);
+    res.cookie('refresh_token', data.refresh_token);
+
+    res.redirect('/rooms');
+  } catch (error) {
+    console.error(error);
+    res.send(error);
+  }
+}
+
+function encode(text) {
+  return new Buffer.from(text).toString('base64');
+}
+
+module.exports = callback;
---
+++ routes/login.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+async function login(req, res) {
+  const baseUrl = 'https://accounts.spotify.com/authorize?response_type=code';
+  const scopes = encodeURIComponent('user-read-private user-read-email');
+  const redirectURI = encodeURIComponent(process.env.SPOTIFY_REDIRECT_URI);
+  const redirectURL = `${baseUrl}&client_id=${process.env.SPOTIFY_CLIENT_ID}&scope=${scopes}&redirect_uri=${redirectURI}`;
+
+  return res.redirect(redirectURL);
+}
+
+module.exports = login;
