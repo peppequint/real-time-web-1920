6253cf26c5e1f26a54f1b490010c2d1fd0997ba6
Peppe-Quint <peppequintb@gmail.com>
2020-04-19 17:06:50 +0200

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
-const index = require('./../controllers/index');
+const index = require('./index');
+const login = require('./login');
+const rooms = require('./rooms');
+
+// middleware
+const callback = require('./../middleware/callback');
 
 app.get('/', index);
+app.get('/login', login);
+app.get('/rooms', rooms);
+
+// middleware route
+app.get('/callback', callback);
 
 module.exports = app;
