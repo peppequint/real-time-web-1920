d4fe5d2ebf62bff9810a3d138911cdbcd5d02152
Peppe-Quint <peppequintb@gmail.com>
2020-04-21 09:28:55 +0200

    detailed room

--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const app = express();
 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 const index = require('./index');
 const login = require('./login');
-const rooms = require('./rooms');
+const dashboard = require('./dashboard');
 
 // middleware
 const callback = require('./../middleware/callback');
 
 app.get('/', index);
 app.get('/login', login);
-app.get('/rooms', rooms);
+app.get('/dashboard', dashboard);
 
 // middleware route
 app.get('/callback', callback);
+
+app.get('/:room', (req, res) => {
+  res.render('pages/room');
+});
 
 module.exports = app;
