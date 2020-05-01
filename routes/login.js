2f8718a324703911f8b24977e40889b0ac94de65
Peppe-Quint <peppequintb@gmail.com>
2020-05-01 13:38:38 +0200

    show dialog

--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,9 @@ require('dotenv').config();
 async function login(req, res) {
   const baseUrl = 'https://accounts.spotify.com/authorize?response_type=code';
   const scopes = encodeURIComponent('user-read-private user-read-email');
+  const dialog = true;
   const redirectURI = encodeURIComponent(process.env.SPOTIFY_REDIRECT_URI);
-  const redirectURL = `${baseUrl}&client_id=${process.env.SPOTIFY_CLIENT_ID}&scope=${scopes}&redirect_uri=${redirectURI}`;
+  const redirectURL = `${baseUrl}&client_id=${process.env.SPOTIFY_CLIENT_ID}&scope=${scopes}&redirect_uri=${redirectURI}&show_dialog=${dialog}`;
 
   return res.redirect(redirectURL);
 }
