bf5bf80526d07818fe323eee07c6b18654eeb1f0
Peppe-Quint <peppequintb@gmail.com>
2020-05-04 22:17:50 +0200

    socket with rooms

--- socket/socket.js
+++ socket/socket.js
@@ -1,40 +1,29 @@
 const { io } = require('../server');
-
-let users = [];
+const userJoin = require('./../controllers/users');
 
 // socket setup
 io.on('connection', (socket) => {
-  console.log(`socket created.`);
+  socket.on('join room', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
 
-  let username = 'username';
+    console.log(user.room);
+
+    socket.on('clicked song', (id) => {
+      const title = id.title;
+      const artist = id.artist;
+
+      socket.emit('server message', `You added ${title} to the vote list.`);
+      io.in(user.room).emit('vote message', { title, artist });
+    });
+
+    socket.on('chat message', (message) => {
+      console.log(message);
+      socket.emit('user message', `${message}`);
+      socket.broadcast.to(user.room).emit('chat message', `${message}`);
+    });
+  });
+
   // send to the single user that's connecting
-  socket.emit('server message', 'new handshake made.');
-
-  socket.on('new user', (id) => {
-    users.push(id);
-
-    username = id;
-
-    console.log(`${username} connected.`);
-
-    socket.emit('server message', `${username} has entered the application.`);
-
-    socket.emit('users list', users);
-  });
-
-  socket.on('clicked song', (id) => {
-    console.log(id);
-    socket.broadcast.emit('server message', `${id} has added to playlist`);
-  });
-
-  // disconnection
-  socket.on('disconnect', (username) => {
-    console.log(username);
-
-    console.log(`${username} has left the app.`);
-
-    console.log('users: ', users);
-
-    socket.broadcast.emit('server message', `${username} has left the application.`);
-  });
+  socket.emit('server message', 'Please, search a number and other users can vote!');
 });
